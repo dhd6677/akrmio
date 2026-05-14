d79df19267c7eb8003b696bc7321d19d0bc55d92
DHD6677 <farmerpalm@126.com>
2026-05-14 22:00:55 +0800

    Add src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app-root">
+    <AppHeader />
+    <main>
+      <HomePage />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import HomePage from './pages/HomePage.vue'
+import AppFooter from './components/AppFooter.vue'
+</script>
